134e2264f7e50d1e3ccc912ae5975cb9119d3690
Wellington Silva <well.silvaa97@gmail.com>
2024-11-02 06:28:30 -0300

    feat: add soonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <Toaster closeButton richColors />
+      <RouterProvider router={router} />
+    </>
+  )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -19,8 +20,13 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.')
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
