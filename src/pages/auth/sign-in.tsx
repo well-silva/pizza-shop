44b2de3223f10658b5d6bc0ebcd712e569f5b8cd
Wellington Silva <well.silvaa97@gmail.com>
2024-11-02 06:24:20 -0300

    feat: add react hook form and zod

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,30 +1,51 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
   return (
-    <>
-      <div className="p-8">
-        <div className="flex w-[350px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas pelo painel do parceiro!
-            </p>
-          </div>
-          <form className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
-            </div>
-            <Button className="w-full" type="submit">
-              Acessar painel
-            </Button>
-          </form>
+    <div className="p-8">
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
         </div>
+
+        <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" {...register('email')} />
+          </div>
+
+          <Button disabled={isSubmitting} className="w-full" type="submit">
+            Acessar painel
+          </Button>
+        </form>
       </div>
-    </>
+    </div>
   )
 }
